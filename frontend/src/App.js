67bb9897952f7766c72706b95e6a0313d47553b1
kayleeburch <kayleemburch@gmail.com>
2024-03-17 16:28:11 -0400

    updating default files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import TaskManager from "./TaskManager";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/about" component={AboutPage} />
+          <Route path="/tasks" component={TaskManager} /> */}
+        <Route path="/" element={<TaskManager />} />
+      </Routes>
+    </Router>
   );
 }
 
